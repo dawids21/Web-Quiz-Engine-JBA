3eff076119b41c1f479a891c0f42c98fae67601b
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-10-09 19:22:24 +0200

    Add post mapping to controller for answering

--- src/main/java/engine/WebQuizController.java
+++ src/main/java/engine/WebQuizController.java
@@ -1,6 +1,8 @@
 package engine;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +12,14 @@ public class WebQuizController {
     public Quiz getQuiz() {
         return new Quiz("The Java Logo", "What is depicted on the Java logo?",
                         new String[]{"Robot", "Tea leaf", "Cup of Coffee", "Bug"});
+    }
+
+    @PostMapping(path = "/api/quiz")
+    public AnswerFeedback answer(@RequestParam int answer) {
+        if (answer == 2) {
+            return new AnswerFeedback(true);
+        } else {
+            return new AnswerFeedback(false);
+        }
     }
 }
