d013bc0e19c0efd40218d6d27e9b60ffac9d3396
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-10-11 19:46:13 +0200

    Write QuizRepository

--- src/main/java/engine/QuizRepository.java
+++ src/main/java/engine/QuizRepository.java
@@ -1,5 +1,25 @@
 package engine;
 
+import java.util.ArrayList;
+
 public class QuizRepository {
 
+    private final ArrayList<Quiz> repository;
+
+    public QuizRepository() {
+        repository = new ArrayList<>();
+    }
+
+    public int add(Quiz quiz) {
+        repository.add(quiz);
+        return repository.size();
+    }
+
+    public Quiz get(int id) {
+        return repository.get(id - 1);
+    }
+
+    public void remove(int id) {
+        repository.remove(id - 1);
+    }
 }
