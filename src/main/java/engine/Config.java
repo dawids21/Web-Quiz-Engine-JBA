1b43a8f262fda999c5e14cc2c4e20108a13c903d
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-10-11 19:56:09 +0200

    Add QuizRepository bean to Config class

--- src/main/java/engine/Config.java
+++ src/main/java/engine/Config.java
@@ -1,8 +1,14 @@
 package engine;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class Config {
+
+    @Bean
+    public QuizRepository getQuizRepository() {
+        return new QuizRepository();
+    }
 
 }
