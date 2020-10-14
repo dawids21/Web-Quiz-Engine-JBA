1d89cae8d4adb2e5f6b355ef11827adc4b4eb43c
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-10-14 07:49:42 +0200

    add necesarry adnotation to QuizService

--- src/main/java/engine/QuizService.java
+++ src/main/java/engine/QuizService.java
@@ -1,10 +1,15 @@
 package engine;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class QuizService {
 
     private final QuizRepository quizRepository;
     private final QuizChecker quizChecker;
 
+    @Autowired
     public QuizService(QuizRepository quizRepository, QuizChecker quizChecker) {
         this.quizRepository = quizRepository;
         this.quizChecker = quizChecker;
