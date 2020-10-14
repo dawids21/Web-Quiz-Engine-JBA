5f3aa67e7bfe95b4f4c42ff9ab2f482d3f180c81
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-10-14 07:54:03 +0200

    Add necessary adnotations to QuizChecker

--- src/main/java/engine/QuizChecker.java
+++ src/main/java/engine/QuizChecker.java
@@ -1,12 +1,16 @@
 package engine;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.Optional;
 
+@Component
 public class QuizChecker {
-
 
     private final QuizRepository repository;
 
+    @Autowired
     public QuizChecker(QuizRepository repository) {
         this.repository = repository;
     }
