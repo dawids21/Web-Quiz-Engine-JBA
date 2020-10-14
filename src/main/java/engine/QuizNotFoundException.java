e8ca2e7da421347a63afa8a863b90a9c877f0595
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-10-14 07:44:50 +0200

    Safe delete QuizNotFoundException

--- src/main/java/engine/QuizNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package engine;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Quiz not found")
-public class QuizNotFoundException extends RuntimeException {
-
-}
