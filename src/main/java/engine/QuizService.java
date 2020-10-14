4a425d2dd104f78504cc04db06c2826b81756c58
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-10-14 07:48:40 +0200

    add fields and constructor to QuizService

--- src/main/java/engine/QuizService.java
+++ src/main/java/engine/QuizService.java
@@ -2,4 +2,11 @@ package engine;
 
 public class QuizService {
 
+    private final QuizRepository quizRepository;
+    private final QuizChecker quizChecker;
+
+    public QuizService(QuizRepository quizRepository, QuizChecker quizChecker) {
+        this.quizRepository = quizRepository;
+        this.quizChecker = quizChecker;
+    }
 }
