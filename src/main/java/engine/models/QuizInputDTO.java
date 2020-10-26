893a1504e5c8018290e6accced8d9e4c13758c27
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-10-26 09:05:57 +0100

    create Quiz Input DTO

---
+++ src/main/java/engine/models/QuizInputDTO.java
@@ -0,0 +1,75 @@
+package engine.models;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+public class QuizInputDTO {
+
+    private String title;
+    private String text;
+    private List<String> options;
+    private Set<Integer> answer = new HashSet<>();
+
+    public QuizInputDTO() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    public Set<Integer> getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(Set<Integer> answer) {
+        this.answer = answer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        QuizInputDTO that = (QuizInputDTO) o;
+        return Objects.equals(getTitle(), that.getTitle()) &&
+               Objects.equals(getText(), that.getText()) &&
+               Objects.equals(getOptions(), that.getOptions()) &&
+               Objects.equals(getAnswer(), that.getAnswer());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getTitle(), getText(), getOptions(), getAnswer());
+    }
+
+    @Override
+    public String toString() {
+        return "QuizInputDTO{" + "title='" + title + '\'' + ", text='" + text + '\'' +
+               ", options=" + options + ", answer=" + answer + '}';
+    }
+}
