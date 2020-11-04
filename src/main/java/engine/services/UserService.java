cd5a2736f33c5fa526157ca613c959623ec41952
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-04 20:48:02 +0100

    add UserService with addUser method

---
+++ src/main/java/engine/services/UserService.java
@@ -0,0 +1,23 @@
+package engine.services;
+
+import engine.models.User;
+import engine.models.UserDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public void addUser(UserDTO userDTO) {
+        var user = new User();
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+    }
+}
