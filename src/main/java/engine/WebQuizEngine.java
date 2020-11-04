24aceaa08d4016a66017b0d9c97746f544547b95
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-04 11:47:29 +0100

    Move ObjectMapper bean to separate config

--- src/main/java/engine/WebQuizEngine.java
+++ src/main/java/engine/WebQuizEngine.java
@@ -1,19 +1,12 @@
 package engine;
 
-import engine.utils.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebQuizEngine {
 
     public static void main(String[] args) {
         SpringApplication.run(WebQuizEngine.class, args);
-    }
-
-    @Bean
-    public ObjectMapper getObjectMapperUtils() {
-        return new ObjectMapper();
     }
 }
