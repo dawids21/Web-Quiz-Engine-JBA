bcee90407f5e35992df77d61507822afb0424ca0
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-05 10:50:51 +0100

    check if email already exists in db

--- src/main/java/engine/services/UserService.java
+++ src/main/java/engine/services/UserService.java
@@ -3,7 +3,9 @@ package engine.services;
 import engine.models.User;
 import engine.models.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 @Component
 public class UserService {
@@ -16,6 +18,11 @@ public class UserService {
     }
 
     public void addUser(UserDTO userDTO) {
+        if (userRepository.existsByEmail(userDTO.getEmail())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                                              "Email " + userDTO.getEmail() +
+                                              " already exists");
+        }
         var user = new User();
         user.setEmail(userDTO.getEmail());
         user.setPassword(userDTO.getPassword());
