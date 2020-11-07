0fd50de216cff0a2ba2d89bfa5a2b0b8b984101a
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-07 17:16:25 +0100

    write PasswordEncryptor implementation

--- src/main/java/engine/utils/PasswordEncryptor.java
+++ src/main/java/engine/utils/PasswordEncryptor.java
@@ -7,9 +7,21 @@ import org.springframework.stereotype.Component;
 @Component
 public class PasswordEncryptor {
 
-    PasswordEncoder passwordEncoder =
+    private final PasswordEncoder passwordEncoder =
              PasswordEncoderFactories.createDelegatingPasswordEncoder();
 
     public PasswordEncryptor() {
+    }
+
+    public String encrypt(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    public boolean matches(String original, String encrypted) {
+        return passwordEncoder.matches(original, encrypted);
+    }
+
+    public boolean upgradeEncoding(String password) {
+        return passwordEncoder.upgradeEncoding(password);
     }
 }
