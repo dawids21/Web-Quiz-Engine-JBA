f87cb5bf3206340ac83afddef288b54d0ef49dc7
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-09 11:09:40 +0100

    return optional from repository after finding by email

--- src/main/java/engine/account/AccountRepository.java
+++ src/main/java/engine/account/AccountRepository.java
@@ -3,10 +3,12 @@ package engine.account;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
 
     Boolean existsByEmail(String email);
 
-    Account findByEmail(String email);
+    Optional<Account> findByEmail(String email);
 }
