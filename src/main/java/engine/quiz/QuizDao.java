6e37146dc0721ae10e004a8adf2e5a518077317a
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-11-11 13:13:52 +0100

    set quiz owner in QuizDao

--- src/main/java/engine/quiz/QuizDao.java
+++ src/main/java/engine/quiz/QuizDao.java
@@ -1,5 +1,6 @@
 package engine.quiz;
 
+import engine.account.AccountRepository;
 import engine.utils.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,20 +15,29 @@ import java.util.stream.StreamSupport;
 public class QuizDao {
 
     private final QuizRepository quizRepository;
+    private final AccountRepository accountRepository;
     private final ObjectMapper objectMapper;
 
     @Autowired
-    public QuizDao(QuizRepository quizRepository, ObjectMapper objectMapper) {
+    public QuizDao(QuizRepository quizRepository, AccountRepository accountRepository,
+                   ObjectMapper objectMapper) {
         this.quizRepository = quizRepository;
+        this.accountRepository = accountRepository;
         this.objectMapper = objectMapper;
     }
 
-    public QuizWithoutAnswerDto addQuiz(QuizInputDto quizInput) {
+    public QuizWithoutAnswerDto addQuiz(QuizInputDto quizInput, String accountEmail) {
         var quiz = objectMapper.mapQuizInputDTOToQuiz(quizInput);
         quiz.getAnswers()
             .forEach(answer -> answer.setQuiz(quiz));
+
         quiz.getOptions()
             .forEach(option -> option.setQuiz(quiz));
+
+        var accountEntity = accountRepository.findByEmail(accountEmail)
+                                             .orElseThrow();
+        accountEntity.addQuiz(quiz);
+
         var quizEntity = quizRepository.save(quiz);
         return objectMapper.mapQuizToQuizDTOWithoutAnswer(quizEntity);
     }
