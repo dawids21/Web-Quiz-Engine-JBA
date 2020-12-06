0a2b28ee75cb286b4dcc618f8b2b16e95c807bd4
Dawid Stasiak <dawid.stasiak21@gmail.com>
2020-12-06 14:33:08 +0100

    create completion entity

---
+++ src/main/java/engine/account/models/CompletionEntity.java
@@ -0,0 +1,89 @@
+package engine.account.models;
+
+import engine.quiz.models.QuizEntity;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.Objects;
+
+@Entity
+@Table(name = "Completions")
+public class CompletionEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "completed_at")
+    private Date completedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "account_id", nullable = false)
+    private AccountEntity accountEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "quiz_id", nullable = false)
+    private QuizEntity quizEntity;
+
+    public CompletionEntity() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCompletedAt() {
+        return completedAt;
+    }
+
+    public void setCompletedAt(Date completedAt) {
+        this.completedAt = completedAt;
+    }
+
+    public AccountEntity getAccountEntity() {
+        return accountEntity;
+    }
+
+    public void setAccountEntity(AccountEntity accountEntity) {
+        this.accountEntity = accountEntity;
+    }
+
+    public QuizEntity getQuizEntity() {
+        return quizEntity;
+    }
+
+    public void setQuizEntity(QuizEntity quizEntity) {
+        this.quizEntity = quizEntity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        CompletionEntity that = (CompletionEntity) o;
+        return getId() == that.getId() &&
+               Objects.equals(getCompletedAt(), that.getCompletedAt()) &&
+               Objects.equals(getAccountEntity(), that.getAccountEntity()) &&
+               Objects.equals(getQuizEntity(), that.getQuizEntity());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getCompletedAt(), getAccountEntity(),
+                            getQuizEntity());
+    }
+
+    @Override
+    public String toString() {
+        return "CompletionEntity{" + "id=" + id + ", completedAt=" + completedAt +
+               ", accountEntity=" + accountEntity + ", quizEntity=" + quizEntity + '}';
+    }
+}
